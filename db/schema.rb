bfdb75bd3ddf224c2b11cc2b3834c92be882080c
Lalith <lalithr95@gmail.com>
2015-12-19 23:12:53 +0530

    Add index action list users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219172145) do
+ActiveRecord::Schema.define(version: 20151219173655) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(version: 20151219172145) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
