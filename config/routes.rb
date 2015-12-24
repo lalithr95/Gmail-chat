f96f6d869d7ed25662c4d86db62eefe17e5f3266
Lalith <lalithr95@gmail.com>
2015-12-24 11:31:33 +0530

    routes and helpers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
+  resources :conversations do
+    resources :messages
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
